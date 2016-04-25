1f7d283b92d7ff8d116ee9805a58a7a76ae2d592
jonasms <jonas.sota@gmail.com>
2016-04-25 15:54:08 -0700

    Fix lineUp function, danceFloor.js

--- src/danceFloor.js
+++ src/danceFloor.js
@@ -6,13 +6,19 @@ $(document).ready(function() {
     //give all dancers a certain position
     var styleSettings = {
       top: '100px',
-      left: '100px'      
+      left: 100      
     };
     //get all dancers
     var $dancers = $('.dancer');
     //give all dancers a position
     $('.dancer').each(function(dancer) {
+      styleSettings.left += 30;
+      styleSettings.left += 'px';
+
+      //TODO
+      // left = left of last object + its width that we saw + some fixed value (5px);
       $(this).css(styleSettings);
+      styleSettings.left = parseInt(styleSettings.left);
     });
   };
 
