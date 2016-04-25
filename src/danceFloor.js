38b57943cd8ba287a1d631cf9e57cee4d67cdfb8
jonasms <jonas.sota@gmail.com>
2016-04-25 16:25:06 -0700

    Add transition to lineUp(), danceFloor.js

--- src/danceFloor.js
+++ src/danceFloor.js
@@ -13,14 +13,28 @@ $(document).ready(function() {
     //give all dancers a position
     $('.dancer').each(function(dancer) {
       styleSettings.left += 30;
-      styleSettings.left += 'px';
 
       //TODO
       // left = left of last object + its width that we saw + some fixed value (5px);
-      $(this).css(styleSettings);
-      styleSettings.left = parseInt(styleSettings.left);
+      //$(this).css(styleSettings);
+      $(this).animate({
+        top: '100px',
+        left: styleSettings.left
+      }, 2000, function() {});
+      // styleSettings.left = parseInt(styleSettings.left);
     });
   };
 
+  
+//  $( "#clickme" ).click(function() {
+//   $( "#book" ).animate({
+//     opacity: 0.25,
+//     top: '100px',
+//     left: '+=50",
+//     height: "toggle"
+//   }, 1000, function() {
+//     // Animation complete.
+//   });
+// });
 
 });
