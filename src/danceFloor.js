1694e172c2e15658ec18be496e340517a8fff3f9
jonasms <jonas.sota@gmail.com>
2016-04-25 18:20:24 -0700

    Develop cotillion(), danceFloor.js

--- src/danceFloor.js
+++ src/danceFloor.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
   $('.lineUpButton').on('click', function() { lineUp(); }); 
+  $('.cotillionButton').on('click', function() { cotillion(); }); 
+  //make another button function here
 
   var lineUp = function() {
     var styleSettings = {
-      top: '100px',
+      top: 100,
       left: 100      
     };
     //give all dancers a new position
@@ -15,24 +17,36 @@ $(document).ready(function() {
       styleSettings.left += 30;
       dancer.danceMode = 'stop'; 
       $('.dancer-' + dancer.id).animate({
-        top: '100px',
+        top: styleSettings.top,
         left: styleSettings.left
       }, 2000);
     });  
-    // dancerNode = $('.dancer-' + dancer.id) ....
 
+  };
 
-    // $('.dancer').each(function(dancer) {
-    //   styleSettings.left += 30;
+  var cotillion = function () {
+    var hasHalved = false;
 
-    //   //TODO
-    //   // left = left of last object + its width that we saw + some fixed value (5px);
-    //   //$(this).css(styleSettings);
-    //   $(this).animate({
-    //     top: '100px',
-    //     left: styleSettings.left
-    //   }, 2000, function() {});
-    // });
+    var styleSettings = {
+      top: 100,
+      left: '100px'      
+    };
+
+    //give each group of dancers the same x-axis, different y-axis
+    _.each(dancers, function(dancer, idx, collection) {
+      dancer.danceMode = 'stop';
+      if ( idx >= (collection.length / 2) && !hasHalved ) {
+      //when half of dancers are linedUp, left += x, reset top
+        styleSettings.left = 400;
+        styleSettings.top = 100;
+        hasHalved = true;
+      }
+      styleSettings.top += 30;
+      $('.dancer-' + dancer.id).animate({
+        top: styleSettings.top,
+        left: styleSettings.left
+      }, 2000);
+    });
   };
 
 });
