a02614ae04a18bfddcc8019fa044b16d3942370c
JonasMS <jonas.sota@gmail.com>
2016-04-26 08:05:31 -0700

    Develop attack function for player

--- src/danceFloor.js
+++ src/danceFloor.js
@@ -1,8 +1,16 @@
+var distanceBetween = function(x1, y1, x2, y2) {
+    return Math.sqrt( Math.pow( x2 - x1, 2 ) + Math.pow( y2 - y1, 2 ) );
+  };
+
+var playerAttack = function (playerId, dancerId) {
+  $('.dancer-' + dancerId).css('border-color', 'green');
+};
+
 $(document).ready(function() {
 
-  $('.lineUp-button').on('click', function() { lineUp(); }); 
+  $('.lineUp-button').on('click', function() { lineUp(); });
   $('.cotillion-button').on('click', function() { cotillion(); });
-  $('.dance-button').on('click', function() { everybodyDance(); });  
+  $('.dance-button').on('click', function() { everybodyDance(); });
   //make another button function here
 
 //--------------------
@@ -10,20 +18,20 @@ $(document).ready(function() {
   var lineUp = function() {
     var styleSettings = {
       top: 100,
-      left: 100      
+      left: 100
     };
     //give all dancers a new position
-    
+
     //iterate through dancer objects
     _.each(dancers, function(dancer) {
       //get html object that has class dancer- + dancer.id
       styleSettings.left += 30;
-      dancer.danceMode = 'stop'; 
+      dancer.danceMode = 'stop';
       $('.dancer-' + dancer.id).animate({
         top: styleSettings.top,
         left: styleSettings.left
       }, 2000);
-    });  
+    });
 
   };
 
@@ -32,7 +40,7 @@ $(document).ready(function() {
 
     var styleSettings = {
       top: 100,
-      left: '100px'      
+      left: '100px'
     };
 
     //give each group of dancers the same x-axis, different y-axis
@@ -67,9 +75,7 @@ $(document).ready(function() {
 
 //--------------------
 //COLLISIONS
-  var distanceBetween = function(x1, y1, x2, y2) {
-    return Math.sqrt( Math.pow( x2 - x1, 2 ) + Math.pow( y2 - y1, 2 ) );
-  };
+
 
   var detectCollisions = function() {
     //iterate thru all objects
@@ -86,6 +92,8 @@ $(document).ready(function() {
     }
   };
 
+  //TODO: need to lookup dancer's id? Why not just use i and j?
+  //Position in dancers array should correlate with id
   var interaction = function (dancer1, dancer2) {
     if ( dancer1.constructor === WalkerDancer ) {
       dancer2.constructor = WalkerDancer;
@@ -96,6 +104,8 @@ $(document).ready(function() {
     }
   };
 
+
+
   setInterval(detectCollisions, 100);
 
 });
@@ -105,13 +115,16 @@ $(document).ready(function() {
 $(document).keydown(function(e) {
   //find the player
   var player;
+
+  var $dancer; //$('.dancer-' + index)
+  //TODO: is this each loop necessary?
   _.each(dancers, function(dancer) {
     if (dancer.constructor === PlayerDancer) {
       player = dancer;
     }
   });
 
-  var pos = $('.dancer-' + player.id).offset(); 
+  var pos = $('.dancer-' + player.id).offset();
   var increment = 10;
   // left pressed
   if (e.which === 37) {
@@ -129,6 +142,29 @@ $(document).keydown(function(e) {
   if (e.which === 40) {
     $('.dancer-' + player.id).css('top', pos.top + increment);
   }
+
+  // spacebar (attack) pressed
+  if (e.which === 32) {
+    //detect collisions between player and all dancers
+    _.each(dancers, function(dancer, idx) {
+      dancer = $('.dancer-' + idx).offset();
+      if ( distanceBetween( pos.left, pos.top, dancer.left, dancer.top) <= 40 ) {
+        if ( idx !== player.id ){
+          playerAttack(player.id, idx);
+        }
+      }
+    });
+  }
 });
+
+//--------------------
+
+
+
+
+
+
+
+
 
 
